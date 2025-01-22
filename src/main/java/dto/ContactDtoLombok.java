03ec4ce02fdb084d57f702d3e39326ab5c2fd70a
MedSveta <sveta1978medved@gmail.com>
2025-01-22 19:42:06 +0200

    StartProject

--- src/main/java/dto/ContactDtoLombok.java
+++ src/main/java/dto/ContactDtoLombok.java
@@ -1,4 +1,30 @@
 package dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+
+//"id": "string",
+//        "name": "string",
+//        "lastName": "string",
+//        "email": "string",
+//        "phone": "7779256054",
+//        "address": "string",
+//        "description": "string"
+
+
 public class ContactDtoLombok {
+    private String id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String description;
 }
--- src/main/java/dto/UserDtoLombok.java
+++ src/main/java/dto/UserDtoLombok.java
@@ -1,4 +1,17 @@
 package dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+
+
+// "username"
+//  "password"
 public class UserDtoLombok {
+    private String username;
+    private String password;
+
 }
