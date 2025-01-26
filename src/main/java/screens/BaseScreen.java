99d1c360f191cdd0741cbec6847cae1617cf355e
MedSveta <sveta1978medved@gmail.com>
2025-01-26 19:46:34 +0200

    Registration Tests

--- src/main/java/screens/BaseScreen.java
+++ src/main/java/screens/BaseScreen.java
@@ -4,6 +4,8 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseScreen {
     AppiumDriver<AndroidElement> driver;
@@ -13,5 +15,31 @@ public class BaseScreen {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
+    public void pause(int time) {
+        try {
+            Thread.sleep(time * 100L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public boolean textInElementPresent(AndroidElement element, String text, int time) {
+        try {
+            return new WebDriverWait(driver, time).until(ExpectedConditions
+                    .textToBePresentInElement(element, text));
+        } catch (org.openqa.selenium.TimeoutException e) {
+            System.out.println("create exception");
+            return false;
+        }
+    }
+
+    public void clickWait(AndroidElement element, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions
+                    .elementToBeClickable(element)).click();
+        } catch (org.openqa.selenium.TimeoutException e) {
+            System.out.println("create exception");
+        }
+    }
 }
+
--- src/main/java/screens/SplashScreen.java
+++ src/main/java/screens/SplashScreen.java
@@ -2,7 +2,10 @@ package screens;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SplashScreen extends BaseScreen{
 
@@ -13,7 +16,18 @@ public class SplashScreen extends BaseScreen{
     @FindBy(id = "com.sheygam.contactapp:id/version_text")
     AndroidElement versionApp;
 
+
     public boolean validateVersionApp(){
         return versionApp.getText().equals("Version 1.0.0");
+    }
+
+    public void goToAuthScreen(int time){
+        try{
+            new WebDriverWait(driver, time)
+                    .until(ExpectedConditions
+                            .presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView")));
+        } catch (Exception e) {
+            System.out.println("element Auth not find");
+        }
     }
 }
